3089f861cdd5d2d7d1515e31e295a731eea77173
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-10 11:05:11 +0900

    first commit

---
+++ src/main/java/com/example/bbsexercise/BbsExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.bbsexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bbsexercise/BbsExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbsexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
