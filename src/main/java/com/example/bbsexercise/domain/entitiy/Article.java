941e1b6e8dec209476fac268b2cc2e40590e7632
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-10 13:34:14 +0900

    [update] 게시물, 댓글 매핑

--- src/main/java/com/example/bbsexercise/domain/entitiy/Article.java
+++ src/main/java/com/example/bbsexercise/domain/entitiy/Article.java
@@ -4,10 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -19,4 +18,7 @@ public class Article {
     private Long id;
     private String title;
     private String content;
+
+    @OneToMany(mappedBy = "article")
+    private List<ArticleReply> replies = new ArrayList<>();
 }
