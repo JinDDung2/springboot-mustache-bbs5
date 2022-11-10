cf781970c1b526488fb577575935858a29eb50f9
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-10 11:52:01 +0900

    [add] ArticleDto

---
+++ src/main/java/com/example/bbsexercise/domain/dto/ArticleDto.java
@@ -0,0 +1,19 @@
+package com.example.bbsexercise.domain.dto;
+
+import com.example.bbsexercise.domain.entitiy.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class ArticleDto {
+    private Long id;
+    private String title;
+    private String content;
+
+    public Article toEntity() {
+        return new Article(id, title, content);
+    }
+}
