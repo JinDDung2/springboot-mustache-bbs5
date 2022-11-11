c4ccd0f818e99c29abdd94dbfc773f37580ecb87
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-11 11:21:29 +0900

    [add] HospitalController 게시물 조회 및 페이징처리

---
+++ src/main/java/com/example/bbsexercise/controller/HospitalController.java
@@ -0,0 +1,31 @@
+package com.example.bbsexercise.controller;
+
+import com.example.bbsexercise.domain.entitiy.Hospital;
+import com.example.bbsexercise.repository.HospitalRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hospitals")
+@RequiredArgsConstructor
+@Slf4j
+public class HospitalController {
+
+    private final HospitalRepository repository;
+
+    @GetMapping("")
+    public String list(Model model, Pageable pageable) {
+        Page<Hospital> hospitals = repository.findAll(pageable);
+        log.info("hospitals.size={}", hospitals.getSize());
+        model.addAttribute("hospitals", hospitals);
+        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+        model.addAttribute("next", pageable.next().getPageNumber());
+        return "hospitals/list";
+    }
+}
