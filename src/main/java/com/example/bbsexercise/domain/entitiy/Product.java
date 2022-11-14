37101e793b6839633bf3a7df76dfc278a86bf805
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-14 11:24:01 +0900

    [Add] Product Entity

---
+++ src/main/java/com/example/bbsexercise/domain/entitiy/Product.java
@@ -0,0 +1,26 @@
+package com.example.bbsexercise.domain.entitiy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
