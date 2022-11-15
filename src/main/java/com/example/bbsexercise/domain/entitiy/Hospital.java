25e8602a830c94c14a93d73befeb29ac0c9fe4ab
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-15 15:37:24 +0900

    [Feat] 생성자 매개변수 businessStatusCode 추가

--- src/main/java/com/example/bbsexercise/domain/entitiy/Hospital.java
+++ src/main/java/com/example/bbsexercise/domain/entitiy/Hospital.java
@@ -37,6 +37,7 @@ public class Hospital {
                 hospital.getPatientRoomCount(),
                 hospital.getTotalNumberOfBeds(),
                 hospital.getBusinessTypeName(),
+                hospital.getBusinessTypeName(),
                 hospital.getTotalAreaSize()
         );
 
