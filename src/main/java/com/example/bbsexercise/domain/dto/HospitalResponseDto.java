28d01c8492ce4a96978856b88f73c77e7ff967e2
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-07 15:55:58 +0900

    [Feat] builder 추가

--- src/main/java/com/example/bbsexercise/domain/dto/HospitalResponseDto.java
+++ src/main/java/com/example/bbsexercise/domain/dto/HospitalResponseDto.java
@@ -6,8 +6,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class HospitalResponseDto {
@@ -20,8 +18,19 @@ public class HospitalResponseDto {
     private String businessStatusName;
     private Float totalAreaSize;
 
-
     public void setBusinessStatusName(String businessStatusName) {
         this.businessStatusName = businessStatusName;
+    }
+
+    @Builder
+    public HospitalResponseDto(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, String businessStatusName, Float totalAreaSize) {
+        this.id = id;
+        this.roadNameAddress = roadNameAddress;
+        this.hospitalName = hospitalName;
+        this.patientRoomCount = patientRoomCount;
+        this.totalNumberOfBeds = totalNumberOfBeds;
+        this.businessTypeName = businessTypeName;
+        this.businessStatusName = businessStatusName;
+        this.totalAreaSize = totalAreaSize;
     }
 }
