c847e6991c78bff03a34359d8a965e162c28b8af
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-08 13:11:26 +0900

    [Feat] POST만 인증/인가 하기

--- src/main/java/com/example/bbsexercise/configuration/SecurityConfig.java
+++ src/main/java/com/example/bbsexercise/configuration/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .cors().and()
                 .authorizeHttpRequests()
                 .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
-                .antMatchers("/api/v1/**").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
